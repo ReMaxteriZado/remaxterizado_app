dc8b3b490f10825d34e050c959ea679df09cf4ae
ReMaxteriZado <alejandro.espinopalma@gmail.com>
2024-03-27 12:06:04 +0000

    Cambios de variables

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-// const defaultApiRoute = "https://remaxterizado_api.test/api";
-const defaultApiRoute = "https://api.remaxterizado.es/api";
+const defaultApiRoute = "https://remaxterizado_api.test/api";
+// const defaultApiRoute = "https://api.remaxterizado.es/api";
 
 export default {
     defaultApiRoute: defaultApiRoute,
